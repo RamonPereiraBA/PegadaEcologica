3693e4ba305ad8707559b6b9b3d5ed87cc164462
gio.spy <82301731+zGi0@users.noreply.github.com>
2022-11-12 15:12:23 -0300

    Update carrousel.js

--- site/carrousel.js
+++ site/carrousel.js
@@ -1,45 +1,57 @@
 var pagina_atual = 0;
 // variavel lista com os membros da equipe
 cards = [{
-  imagem_card: "Imagens/PEA.jpg",
+  imagem_card: ["Imagens/logos/PEA.jpg", 45, 30],
   nome_organizacao_card: "Programa Educação Ambiental",
   funcionalidade_card: "Colaboradores",
-  texto_equipe_card: "Temos o objetivo de promover a educação ambiental junto à população. Visando o conhecimento ecológico necessário para a proteção e preservação ambiental.",
-  },
+  texto_equipe_card: "Temos o objetivo de promover a educação ambiental junto à população. Visando o conhecimento ecológico necessário para a proteção e preservação ambiental.",  
+},
   {
-  imagem_card: "imagens/Logo fundacao.png",
+  imagem_card: ["Imagens/logos/Logo fundacao.png", 45, 30],
   nome_organizacao_card: "Fundação CSN",
   funcionalidade_card: "Cordenadores",
   texto_equipe_card: "Somos responsáveis pelas ações sociais do Grupo CSN e estamos presentes nas principais cidades em que a empresa atua. Os pilares que sustentam a nossa atuação são educação, cultura, articulação e curadoria.",
   },
   {
-  imagem_card: "Imagens/images.png",
+  imagem_card: ["Imagens/logos/inpe-logo.png", 45, 30],
   nome_organizacao_card: "INPE",
   funcionalidade_card: "Créditos",
-  texto_equipe_card: "",
+  texto_equipe_card: "Criada em 1961 com o objetivo de impulsionar o país nas pesquisas cientificas e nas tecnologias espaciais. Suas atividades se ampliaram e a importância dos estudos vão desde assuntos complexos sobre a origem do Universo até as questões de desflorestamento das nossas matas.",
   },
   {
-  imagem_card: "imagens/etpc.png",
+  imagem_card: ["Imagens/logos/etpc.png", 45, 30],
   nome_organizacao_card: "Escola Técnica Pandiá Calógeras",
   funcionalidade_card: "Desenvolvedores",
   texto_equipe_card: "Fundada em 1944 visando oferecer ensino técnico aos primeiros empregados da Companhia Siderúrgica Nacional e aos seus filhos, é referência nacional em educação para o trabalho, pela excelência de seu processo ensino-aprendizagem, atualização tecnológica e investimento social.",
   }
 ]
+  
 
 function passar_pagina(){
-  // Pegando os elementos do HTML
+
+  // componentes do card
   var imagem = document.getElementById("imagem");
-  var nome_organizacao = document.getElementById('nome_organização');
+  var nome_organizacao = document.getElementById('nome_organizacao');
   var funcionalidade = document.getElementById('funcionalidade');
   var texto_equipe =  document.getElementById('texto_equipe');
+
   // Passando a página
   pagina_atual++;
   if (pagina_atual >= cards.length){
     pagina_atual = 0;
   }
+
   //Setando as configurações
-  imagem.setAttribute('src', cards[pagina_atual].imagem_card);
+  imagem.setAttribute('src', cards[pagina_atual].imagem_card[0]);
   nome_organizacao.innerText = cards[pagina_atual].nome_organizacao_card;
   funcionalidade.innerText = cards[pagina_atual].funcionalidade_card;
   texto_equipe.innerText = cards[pagina_atual].texto_equipe_card;
+  
+  // fazendo com que o texto encaixe no card
+  texto_equipe.wordBreak = "break-all"
+
+  // setando altura e largura
+  imagem.setAttribute('width', cards[pagina_atual].imagem_card[1]);
+  imagem.setAttribute('height', cards[pagina_atual].imagem_card[2]);
+  
 }
