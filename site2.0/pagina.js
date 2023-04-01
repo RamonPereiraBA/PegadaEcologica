34d4c114b72ece92cc419d10f566025f03fe6844
Daniel Bernardes Cardoso <cqrdosodaniel@gmail.com>
2023-03-31 23:06:34 -0300

    Update pagina.js

--- site2.0/pagina.js
+++ site2.0/pagina.js
@@ -2,7 +2,7 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            entry.target.classList.add('show_animacao');
         }
     });
 });
@@ -28,6 +28,7 @@ const lista_lugares = [
     document.getElementById("objetivo"),
     document.getElementById("pilares"),
     document.getElementById("equipe"),
+    document.getElementById("desenvolvedores"),
     document.getElementById("pagina_quiz"),
 ];
 // é preciso diminuir em 30 para ajustar a posição
@@ -36,6 +37,7 @@ const lista_posicoes = [
     parseInt(window.getComputedStyle(document.getElementById("objetivo")).top) - 30,
     parseInt(window.getComputedStyle(document.getElementById("pilares")).top) - 30,
     parseInt(window.getComputedStyle(document.getElementById("equipe")).top) - 30,
+    parseInt(window.getComputedStyle(document.getElementById("desenvolvedores")).top) - 30,
     parseInt(window.getComputedStyle(document.getElementById("pagina_quiz")).top) - 30,
 ];
 
@@ -45,13 +47,41 @@ function rolar_pagina(lugar){
     setTimeout(() => lista_lugares[lugar].style.animation = "destaque_pagina 0.5s linear", 500);
 }
 
+// configurando o dropdown
+const botao_ir_objetivo = document.getElementById("ir_objetivo")
+const botao_ir_pilar = document.getElementById("ir_pilares")
+const botao_ir_equipe = document.getElementById("ir_equipe")
+const botao_ir_desenvolvedores = document.getElementById("ir_desenvolvedores")
+
+botao_ir_objetivo.addEventListener('click', ()=> rolar_pagina(1))
+botao_ir_pilar.addEventListener('click', ()=> rolar_pagina(2))
+botao_ir_equipe.addEventListener('click', ()=> rolar_pagina(3))
+botao_ir_desenvolvedores.addEventListener('click', ()=> rolar_pagina(4))
+
 // configurando a seta final
 const seta_cima = document.getElementById("seta_cima")
 seta_cima.addEventListener('click', ()=> rolar_pagina(0))
 
 // configurando o botão inicial
 const botao_inicial = document.getElementById("btn_começar");
-botao_inicial.addEventListener('click', () => rolar_pagina(4))
+botao_inicial.addEventListener('click', () => rolar_pagina(5))
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Mudando a cor da nav bar
+const lista_cores_navbar = ['#9FC131', '#042940', '#13678A', '#9AEBA3', '#45C4B0', '#042940']
+const navbar = document.getElementById('navBar');
+
+function mudar_cor_navbar() {
+    for (let x=0; x<5;x++){
+        if(window.scrollY >= lista_posicoes[x]){
+            navbar.style.backgroundColor = lista_cores_navbar[x];
+        }
+    }
+}
+
+mudar_cor_navbar();
+
+setInterval(mudar_cor_navbar, 1500);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Criando as caixas dos pilares
