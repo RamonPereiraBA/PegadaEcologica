4f914a09a0fa0c9ada392fafab99ad6a1aecb625
Daniel Bernardes Cardoso <cqrdosodaniel@gmail.com>
2023-04-07 21:22:56 -0300

    Update resultadoDados.php

--- site2.0/html/resultadoDados.php
+++ site2.0/html/resultadoDados.php
@@ -1,3 +1,21 @@
+<?php
+    $listinha = array(array("q" => "Com que frequência você come carne vermelha?", "1" => "come carne", "2" =>"come sempre", "3"=> "come peixe", "4"=>"cansei de escrever", "5"=>""),
+     array("q" => "Com que frequência você come peixe ou frutos do mar?", "1" => "come carne", "2" =>"come sempre", "3"=> "come peixe", "4"=>"cansei de escrever", "5"=>"sla"), 
+     array("q" => "Você usa ar condicionado ou aquecedor na sua casa?", "1" => "come carne", "2" =>"come sempre", "3"=> "come peixe", "4"=>"cansei de escrever", "5"=>""),
+     array("q" => "Qual a procedência dos alimentos que você consome?", "1" => "come carne", "2" =>"come sempre", "3"=> "come peixe", "4"=>"cansei de escrever", "5"=>""),
+     array("q" => "Quantas vezes por ano você compra roupas novas?", "1" => "come carne", "2" =>"come sempre", "3"=> "come peixe", "4"=>"cansei de escrever", "5"=>""),
+     array("q" => "Com que frequência você compra equipamentos eletrônicos?", "1" => "come carne", "2" =>"come sempre", "3"=> "come peixe", "4"=>"cansei de escrever", "5"=>""),
+     array("q" => "Com que frequência você compra livros e jornais?", "1" => "come carne", "2" =>"come sempre", "3"=> "come peixe", "4"=>"cansei de escrever", "5"=>""),
+     array("q" => "Como você descarta o lixo da sua casa?", "1" => "come carne", "2" =>"come sempre", "3"=> "come peixe", "4"=>"cansei de escrever", "5"=>""),
+     array("q" => "Usa lâmpadas econômicas?", "1" => "come carne", "2" =>"come sempre", "3"=> "come peixe", "4"=>"cansei de escrever", "5"=>""),
+     array("q" => "Que meio de transporte você mais usa?", "1" => "come carne", "2" =>"come sempre", "3"=> "come peixe", "4"=>"cansei de escrever", "5"=>""),
+     array("q" => "Com que frequência você bebe refrigerante?", "1" => "come carne", "2" =>"come sempre", "3"=> "come peixe", "4"=>"cansei de escrever", "5"=>""),
+     array("q" => "Quanto tempo você gasta no banho diariamente?", "1" => "come carne", "2" =>"come sempre", "3"=> "come peixe", "4"=>"cansei de escrever", "5"=>""),
+     array("q" => "Quantas horas você gasta viajando de avião anualmente?", "1" => "come carne", "2" =>"come sempre", "3"=> "come peixe", "4"=>"cansei de escrever", "5"=>""),
+     array("q" => "Qual a quantidade de alimentos que você consome que contém açúcar refinado?", "1" => "come carne", "2" =>"come sempre", "3"=> "come peixe", "4"=>"cansei de escrever", "5"=>""),
+    );
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -20,18 +38,18 @@
         <button id="irInicio">Retornar ao início</button>
         <button id="RefazerQuiz">Voltar ao QUIZ</button>
     </div>
-    <div id="textos">
+    <div class="textos">
         <?php 
-            for ($x = 1; $x <= 14; $x++){ ?>
-                <div id="questao">
-                    <div id="tituloQuestao"></div>
-                    <div id="op1" class="option"></div>
-                    <div id="op2" class="option"></div>
-                    <div id="op3" class="option"></div>
-                    <div id="op4" class="option"></div>
-                    <div id="op5" class="option"></div>
+            for ($x = 0; $x <= 13; $x++){ ?>
+                <div class="questao">
+                    <div id="tituloQuestao"><?= $listinha[$x]["q"] ?></div>
+                    <div id="op1" class="option"><?= $listinha[$x]["1"] ?></div>
+                    <div id="op2" class="option"><?= $listinha[$x]["2"] ?></div>
+                    <div id="op3" class="option"><?= $listinha[$x]["3"] ?></div>
+                    <div id="op4" class="option"><?= $listinha[$x]["4"] ?></div>
+                    <div id="op5" class="option"><?= $listinha[$x]["5"] ?></div>
                 </div>
-            <?php }?>        
+         <?php }?>        
     </div>
 </body>
 </html>
