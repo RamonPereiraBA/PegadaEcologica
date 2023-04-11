ea014bed74eb018db12fcdab1f0ea3b8b8e265e7
Daniel Bernardes Cardoso <cqrdosodaniel@gmail.com>
2023-04-11 19:20:01 -0300

    Update resultadoDados.js

--- site2.0/resultadoDados.js
+++ site2.0/resultadoDados.js
@@ -11,3 +11,20 @@ function ir_inicio(){
 function ir_resultado(){
     location.href = "./resultado.html"
 }
+
+///////////////////////////////////////////////////////
+var mediaGlobal = document.getElementById("numero_media");
+var numeroMediGlobal = parseInt(mediaGlobal.innerText);
+if (numeroMediGlobal >= 50){
+
+}else if (numeroMediGlobal >= 35 && numeroMediGlobal <= 49){
+    document.documentElement.style.setProperty('--cor_caixa_titulo', '#FFAE00');
+    document.documentElement.style.setProperty('--cor_caixa_resultado', '#C43302');
+    document.documentElement.style.setProperty('--cor_titulo_resultado', '#010221');
+    document.documentElement.style.setProperty('--cor_caixa_geral', '#B7BF99');
+}else{
+    document.documentElement.style.setProperty('--cor_caixa_titulo', '#D92929');
+    document.documentElement.style.setProperty('--cor_caixa_resultado', '#260101');
+    document.documentElement.style.setProperty('--cor_titulo_resultado', '#010221');
+    document.documentElement.style.setProperty('--cor_caixa_geral', '#B0BFBE');
+}
