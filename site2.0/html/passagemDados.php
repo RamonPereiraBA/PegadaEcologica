29026d4fa8b1b9fe82ba776c76d6a165e4c65621
Daniel Bernardes Cardoso <cqrdosodaniel@gmail.com>
2023-06-03 21:25:34 -0300

    adicionando html para remover cache

--- site2.0/html/passagemDados.php
+++ site2.0/html/passagemDados.php
@@ -40,6 +40,12 @@ if (isset($_POST['botao'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta
+      http-equiv="Cache-Control"
+      content="no-cache, no-store, must-revalidate"
+    />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
     <title>Cadastro</title>
 	<link rel="icon" href="../Imagens/logosite.png">
     <link rel="stylesheet" href="../css/passagemDados.css">
