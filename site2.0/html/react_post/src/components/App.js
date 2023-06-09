ce95801e288955cb3fbe4b782e3cb12090b490fe
Daniel Bernardes Cardoso <cqrdosodaniel@gmail.com>
2023-06-08 21:46:04 -0300

    finalizado json tela posts

--- site2.0/html/react_post/src/components/App.js
+++ site2.0/html/react_post/src/components/App.js
@@ -1,64 +1,20 @@
 import React from "react";
-
-var lista_posts = [
-    {
-        "nome_do_post": "Post 1",
-        "link_imagem": "https://media.licdn.com/dms/image/D4D22AQE6_1censMOvA/feedshare-shrink_800/0/1682281663348?e=1687392000&v=beta&t=WiqMwAUTCgs5ZG5efMrPAPVK-ETpSIoFDXez0kk9YqE",
-        "data": "15-05-2018",
-        "link_post": "",
-        "secao": "ecologia" 
-      },
-      {
-        "nome_do_post": "Post 2",
-        "link_imagem": "https://media.licdn.com/dms/image/D4D22AQE6_1censMOvA/feedshare-shrink_800/0/1682281663348?e=1687392000&v=beta&t=WiqMwAUTCgs5ZG5efMrPAPVK-ETpSIoFDXez0kk9YqE",
-        "data": "15-05-2018",
-        "link_post": "",
-        "secao": "projeto" 
-      },
-      {
-        "nome_do_post": "Post 3",
-        "link_imagem": "https://media.licdn.com/dms/image/D4D22AQE6_1censMOvA/feedshare-shrink_800/0/1682281663348?e=1687392000&v=beta&t=WiqMwAUTCgs5ZG5efMrPAPVK-ETpSIoFDXez0kk9YqE",
-        "data": "15-05-2018",
-        "link_post": "",
-        "secao": "ecologia" 
-      },
-      {
-        "nome_do_post": "Post 4",
-        "link_imagem": "https://media.licdn.com/dms/image/D4D22AQE6_1censMOvA/feedshare-shrink_800/0/1682281663348?e=1687392000&v=beta&t=WiqMwAUTCgs5ZG5efMrPAPVK-ETpSIoFDXez0kk9YqE",
-        "data": "15-05-2018",
-        "link_post": "",
-        "secao": "projeto" 
-      },
-      {
-        "nome_do_post": "Post 5",
-        "link_imagem": "https://media.licdn.com/dms/image/D4D22AQE6_1censMOvA/feedshare-shrink_800/0/1682281663348?e=1687392000&v=beta&t=WiqMwAUTCgs5ZG5efMrPAPVK-ETpSIoFDXez0kk9YqE",
-        "data": "15-05-2018",
-        "link_post": "",
-        "secao": "ecologia" 
-      },
-      {
-        "nome_do_post": "Post 6",
-        "link_imagem": "https://media.licdn.com/dms/image/D4D22AQE6_1censMOvA/feedshare-shrink_800/0/1682281663348?e=1687392000&v=beta&t=WiqMwAUTCgs5ZG5efMrPAPVK-ETpSIoFDXez0kk9YqE",
-        "data": "15-05-2018",
-        "link_post": "",
-        "secao": "programação do site" 
-      }
-]
+import Posts_json from "../posts.json";
 
 function App(){
     function Post(props){
         return (
             <>
-                <h2>Sobre {props.titulo}</h2>
-                <section class="divPost">
-                  <div class="scrollPost">
-                    {lista_posts.map((post_atual) => (
+                <h2>{props.titulo}</h2>
+                <section className="divPost">
+                  <div className="scrollPost">
+                    {Posts_json.map((post_atual) => (
                     <>
                         { props.titulo === post_atual.secao &&(
                         <>
-                        <div class="post">
+                        <div className="post">
                           <img src={post_atual.link_imagem} />
-                          <a href="https://media.licdn.com/dms/image/D4D22AQE6_1censMOvA/feedshare-shrink_800/0/1682281663348?e=1687392000&v=beta&t=WiqMwAUTCgs5ZG5efMrPAPVK-ETpSIoFDXez0kk9YqE">{post_atual.nome_do_post}</a>
+                          <a href={post_atual.link_post}>{post_atual.nome_do_post}</a>
                           <p>{post_atual.data}</p>
                         </div>
                         </>
@@ -75,9 +31,10 @@ function App(){
         <>
             <h1>Posts</h1>
             <p>A seguir estão, de forma organizada, os nossos posts do INSTAGRAM.</p>
-            <Post titulo="ecologia" nome_div="ecologia"/>
-            <Post titulo="projeto" nome_div="projeto"/>
-            <Post titulo="programação do site" nome_div="programacao" />
+            <Post titulo="Sobre ecologia"/>
+            <Post titulo="Sobre projeto"/>
+            <Post titulo="Sobre programação do site" />
+            <Post titulo="Nossos especiais"/>
         </>
     )
 
