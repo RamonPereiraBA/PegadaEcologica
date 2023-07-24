b8d621a2b6e13bc0654010d7c6d178d602f11079
Giovanni Almeida <82301731+zGi0@users.noreply.github.com>
2023-07-24 20:36:42 -0300

    tirando o erro no arquivo

--- site2.0/conexao_servidor.php
+++ site2.0/conexao_servidor.php
@@ -1,12 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
+$servername = "host";
+$username = "user";
 $password = "senha";
 $dbname = "pegadaecologica";
 
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch(PDOException $e) {
