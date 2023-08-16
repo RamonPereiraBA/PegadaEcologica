fae8d9a576e1758a94fd9004e66b294c49342fbc
Daniel Bernardes Cardoso <cqrdosodaniel@gmail.com>
2023-08-16 20:54:54 -0300

    Update passagemDados.js

--- site2.0/passagemDados.js
+++ site2.0/passagemDados.js
@@ -1,29 +1,15 @@
-// const campo_email = document.getElementById("input_email");
-// function verificar_radioButton(parametro){
-//     if (parametro == "sim"){
-//         formulario.style.display = "block";
-//     }else{
-//         formulario.style.display = "none";
-//         campo_email.value = "";
-//     }
-//     btContinuar.style.display = "block"
-// }
+const bt_continuar =  document.getElementById("botao");
 // Primeiro, obtenha uma referência para o elemento select pelo ID
-console.log("aaa")
 var containerEl = document.getElementById("ocupacao_id"); // container
 var elem = containerEl.getElementsByTagName("option"); // elemento
 
 containerEl.addEventListener("change", function() {
     // O código dentro desta função será executado quando o valor mudar
-    var valorSelecionado = elem; // objetivo: pegar o valor selecionado
-    if (valorSelecionado == "nao_escolhido")
-    {
-
+    var valorSelecionado = containerEl.value; // objetivo: pegar o valor selecionado
+    if (valorSelecionado === "nao_escolhido"){
+        bt_continuar.style.display = "none";
+        return;
     }
-    console.log(valorSelecionado)
     
-    // Agora você pode fazer algo com o novo valor selecionado
-    // btContinuar.style.display = "block"
-
-    console.log("Novo valor selecionado: " + valorSelecionado);
+    bt_continuar.style.display = "block"
 });
