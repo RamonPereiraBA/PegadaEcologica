31b76dca280a1f4b1a7622251bce19fe7bfd63ad
Daniel Bernardes Cardoso <cqrdosodaniel@gmail.com>
2023-09-03 23:09:27 -0300

    terminando o form

--- site2.0/html/passagemDados.php
+++ site2.0/html/passagemDados.php
@@ -1,5 +1,5 @@
 <?php
-function mandar_banco_dados($ocupacao, $data, $total, $questoes){
+function mandar_banco_dados($bairro, $unidade, $data, $total, $questoes){
     require('../conexao_servidor.php');
     
     $stmt = $conn->prepare("INSERT INTO tabelaecologica (total, questoes) VALUES (:valor1, :valor2)");
@@ -9,10 +9,11 @@ function mandar_banco_dados($ocupacao, $data, $total, $questoes){
     
     $stmt->execute();
      
-    $stmt2 = $conn->prepare("INSERT INTO tabelainfo (ocupacao, dia) VALUES (:valor1, :valor2)");
+    $stmt2 = $conn->prepare("INSERT INTO tabelainfo (bairro, unidade, dia) VALUES (:valor1, :valor2, :valor3");
     
-    $stmt2->bindValue(':valor1', $ocupacao);
-    $stmt2->bindValue(':valor2', $data);
+    $stmt2->bindValue(':valor1', $bairro);
+    $stmt2->bindValue(':valor2', $unidade);
+    $stmt2->bindValue(':valor3', $data);
     
     $stmt2->execute();
 }
@@ -40,9 +41,8 @@ function somarTotal($questoes){
 }
 
 if ($_SERVER["REQUEST_METHOD"] === "POST" and isset($_POST['bairro'])){
-    echo ("foi");
-    return;
-    $ocupacao_variavel = $_POST['ocupacao'];
+    $bairro_variavel = $_POST['bairro'];
+    $unidade_variavel = $_POST['unidade'];
     $data_atual = date("Y-m-d");
     $questoes_variavel = $_POST['questoes'];
     $dica_variavel = $_POST['dica'];
@@ -55,7 +55,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" and isset($_POST['bairro'])){
         exit();
     }
     $total_variavel = somarTotal($questoes_variavel);
-    mandar_banco_dados($ocupacao_variavel, $data_atual, $total_variavel, $questoes_variavel);
+    mandar_banco_dados($bairro_variavel, $unidade_variavel, $data_atual, $total_variavel, $questoes_variavel);
     header('Location: resultado.html?total='. $total_variavel . "&dica=" . $dica_variavel);
     exit();
 }
@@ -179,7 +179,6 @@ button:active
         <input type="hidden" name="questoes" value="<?= $_POST['resultado_questoes'] ?>">
         <input type="hidden" name="total" value="<?= $_POST['resultado_total'] ?>">
         <input type="hidden" name="dica" value="<?= $_POST['resultado_dica'] ?>">
-        <input type="hidden" name="morador" value="">
         <input type="hidden" name="bairro" value="">
         <input type="hidden" name="unidade" value="">
     </form>
@@ -213,10 +212,111 @@ button:active
             <button onclick="confirmar_2()">Continuar</button>
         </div>
         <div id="form_3_id" style="display: none;">
-            <h3>Informe seu bairro</h3>
-            <input type="radio" value="s" name="resposta_form_2">Sim<br>
-            <input type="radio" value="n" name="resposta_form_2">Não<br>
-            <button onclick="confirmar_2()">Continuar</button>
+            <label for="bairro_dropdown">Informe seu bairro</label>
+            <select id="bairro_dropdown_id" name="bairro_dropdown">
+                <option value="bairro">bairro</option>
+            </select>
+            <label for="departamentos">Trabalha na CSN? Nos informe sua Unidade Organizacional</label>
+            <select id="departamentos" name="departamentos">
+                <option value="COORDENACAO DE ENGENHARIA">COORDENACAO DE ENGENHARIA</option>
+                <option value="DIRETORIA EXECUTIVA PRODUCAO SIDERURGIA">DIRETORIA EXECUTIVA PRODUCAO SIDERURGIA</option>
+                <option value="GERENCIA TRANSPORTE FERROVIARIO INTERNO">GERENCIA TRANSPORTE FERROVIARIO INTERNO</option>
+                <option value="COORDENACAO DE ENGENHARIA EQUIPAMENTOS">COORDENACAO DE ENGENHARIA EQUIPAMENTOS</option>
+                <option value="GERENCIA DE MANUTENCAO DO LTQ">GERENCIA DE MANUTENCAO DO LTQ</option>
+                <option value="COORDENACAO INSP MECANICA ALTOS FORNOS">COORDENACAO INSP MECANICA ALTOS FORNOS</option>
+                <option value="GERENCIA GERAL PLANEJAMENTO E DESEMPENHO">GERENCIA GERAL PLANEJAMENTO E DESEMPENHO</option>
+                <option value="GERENCIA DE CILINDROS">GERENCIA DE CILINDROS</option>
+                <option value="GERENCIA DE PROCESSOS DA LAMINACAO">GERENCIA DE PROCESSOS DA LAMINACAO</option>
+                <option value="COORDENACAO DE GESTAO DE MANUTENCAO">COORDENACAO DE GESTAO DE MANUTENCAO</option>
+                <option value="GERENCIA DE OFICINAS MECANICAS">GERENCIA DE OFICINAS MECANICAS</option>
+                <option value="GERENCIA DE LABORATORIO">GERENCIA DE LABORATORIO</option>
+                <option value="GERENCIA DE MANUTENCAO E TRANSPORTE">GERENCIA DE MANUTENCAO E TRANSPORTE</option>
+                <option value="GERENCIA DE PROJETOS METALURGIA DO ACO">GERENCIA DE PROJETOS METALURGIA DO ACO</option>
+                <option value="GERENCIA DE MANUTENCAO">GERENCIA DE MANUTENCAO</option>
+                <option value="GERENCIA DE PROG CONTR E ABASTECIMENTO">GERENCIA DE PROG CONTR E ABASTECIMENTO</option>
+                <option value="COORDENACAO DE PROJETOS ESPECIAIS">COORDENACAO DE PROJETOS ESPECIAIS</option>
+                <option value="GERENCIA DE ENTREPOSTOS E ESCOAMENTO">GERENCIA DE ENTREPOSTOS E ESCOAMENTO</option>
+                <option value="COORDENACAO INSPECAO SINTERIZACAO 4">COORDENACAO INSPECAO SINTERIZACAO 4</option>
+                <option value="GERENCIA DE PREV E COMBATE A INCENDIO">GERENCIA DE PREV E COMBATE A INCENDIO</option>
+                <option value="GERENCIA DE SUPORTE OPERACIONAL">GERENCIA DE SUPORTE OPERACIONAL</option>
+                <option value="GERENCIA TECNOLOGIA DE AUTOMACAO">GERENCIA TECNOLOGIA DE AUTOMACAO</option>
+                <option value="GERENCIA DE MANUTENCAO ELETRICA">GERENCIA DE MANUTENCAO ELETRICA</option>
+                <option value="GERENCIA DE MANUTENCAO REFRATARIA">GERENCIA DE MANUTENCAO REFRATARIA</option>
+                <option value="GERENCIA DE PATIO DE MATERIAS PRIMAS">GERENCIA DE PATIO DE MATERIAS PRIMAS</option>
+                <option value="GERENCIA DE ALTOS FORNOS">GERENCIA DE ALTOS FORNOS</option>
+                <option value="COORDENACAO DE PLANEJAMENTO E CONTROLE">COORDENACAO DE PLANEJAMENTO E CONTROLE</option>
+                <option value="GERENCIA DE PROJETOS SINTERIZACAO">GERENCIA DE PROJETOS SINTERIZACAO</option>
+                <option value="COORDENACAO DE PROJETOS ESPECIAIS">COORDENACAO DE PROJETOS ESPECIAIS</option>
+                <option value="GERENCIA DE PROJETOS COQUERIA">GERENCIA DE PROJETOS COQUERIA</option>
+                <option value="COORDENACAO DE PLANEJAMENTO E CONTROLE">COORDENACAO DE PLANEJAMENTO E CONTROLE</option>
+                <option value="GERENCIA DE REDUCAO A FRIO LTF 3">GERENCIA DE REDUCAO A FRIO LTF 3</option>
+                <option value="GERENCIA DE RECOZIMENTO E ACABAMENTO">GERENCIA DE RECOZIMENTO E ACABAMENTO</option>
+                <option value="GERENCIA DE AGUAS E EFLUENTES">GERENCIA DE AGUAS E EFLUENTES</option>
+                <option value="GERENCIA DE LINGOTAMENTO CONTINUO">GERENCIA DE LINGOTAMENTO CONTINUO</option>
+                <option value="GERENCIA GERAL  DE PLANEJAMENTO E LOGISTICA">GERENCIA GERAL  DE PLANEJAMENTO E LOGISTICA</option>
+                <option value="GERENCIA GERAL DE DESENV DE PRODUTOS">GERENCIA GERAL DE DESENV DE PRODUTOS</option>
+                <option value="GERENCIA DE LAMINADOS A QUENTE">GERENCIA DE LAMINADOS A QUENTE</option>
+                <option value="GERENCIA DE PROJETOS LAMINACAO">GERENCIA DE PROJETOS LAMINACAO</option>
+                <option value="DIRETORIA DE METALURGIA">DIRETORIA DE METALURGIA</option>
+                <option value="GERENCIA DE LAMINADOS A FRIO">GERENCIA DE LAMINADOS A FRIO</option>
+                <option value="GERENCIA MANUT GALVANIZ E LAMINADOS FRIO">GERENCIA MANUT GALVANIZ E LAMINADOS FRIO</option>
+                <option value="GERENCIA DE ALTOS FORNOS">GERENCIA DE ALTOS FORNOS</option>
+                <option value="GERENCIA DESENV PRODUTOS UPV">GERENCIA DESENV PRODUTOS UPV</option>
+                <option value="GERENCIA DE MANUTENCAO ELETROMECANICA">GERENCIA DE MANUTENCAO ELETROMECANICA</option>
+                <option value="GERENCIA DE PREP ARMAZENAGEM BOBINAS">GERENCIA DE PREP ARMAZENAGEM BOBINAS</option>
+                <option value="GERENCIA DE DECAPAGEM ACIDA">GERENCIA DE DECAPAGEM ACIDA</option>
+                <option value="GERENCIA GERAL LAMIN FRIO GALV DECAPAGEM">GERENCIA GERAL LAMIN FRIO GALV DECAPAGEM</option>
+                <option value="GERENCIA DE ESTANHAMENTO">GERENCIA DE ESTANHAMENTO</option>
+                <option value="GERENCIA DE ENCRUAMENTO">GERENCIA DE ENCRUAMENTO</option>
+                <option value="GERENCIA DE PONTE ROLANTE">GERENCIA DE PONTE ROLANTE</option>
+                <option value="GERENCIA DE MANUT DA METALURGIA DO ACO">GERENCIA DE MANUT DA METALURGIA DO ACO</option>
+                <option value="GERENCIA DE MOVIMENTACAO DE PRODUTO">GERENCIA DE MOVIMENTACAO DE PRODUTO</option>
+                <option value="GERENCIA DE CALCINACAO">GERENCIA DE CALCINACAO</option>
+                <option value="GERENCIA MANUTENCAO DE EXECUCAO REDUCAO">GERENCIA MANUTENCAO DE EXECUCAO REDUCAO</option>
+                <option value="GERENCIA DE DISTRIBUICAO DE ENERGETICO">GERENCIA DE DISTRIBUICAO DE ENERGETICO</option>
+                <option value="GERENCIA DE CARBOQUIMICOS E CALCINACAO">GERENCIA DE CARBOQUIMICOS E CALCINACAO</option>
+                <option value="GERENCIA DE GARANTIA DA QUALIDADE">GERENCIA DE GARANTIA DA QUALIDADE</option>
+                <option value="GERENCIA DE RECOZIMENTO E ZINCAGEM">GERENCIA DE RECOZIMENTO E ZINCAGEM</option>
+                <option value="GERENCIA MANUT LINGOTAMENTO CONTINUO">GERENCIA MANUT LINGOTAMENTO CONTINUO</option>
+                <option value="GERENCIA GERAL DE EXECUCAO">GERENCIA GERAL DE EXECUCAO</option>
+                <option value="GERENCIA DE INSPECAO MECANICA REFRATAR">GERENCIA DE INSPECAO MECANICA REFRATAR</option>
+                <option value="GERENCIA DE SINTERIZACOES">GERENCIA DE SINTERIZACOES</option>
+                <option value="GERENCIA DE MANUTENCAO DE UTILIDADES">GERENCIA DE MANUTENCAO DE UTILIDADES</option>
+                <option value="GERENCIA GERAL DE LAMINADOS A QUENTE">GERENCIA GERAL DE LAMINADOS A QUENTE</option>
+                <option value="GERENCIA LAMINACAO QUENTE FRIO">GERENCIA LAMINACAO QUENTE FRIO</option>
+                <option value="COORDENACAO DE PLANEJAMENTO E CONTROLE">COORDENACAO DE PLANEJAMENTO E CONTROLE</option>
+                <option value="COORDENACAO DE DESEMPENHO DE CONTRATOS">COORDENACAO DE DESEMPENHO DE CONTRATOS</option>
+                <option value="COORDENACAO PLANEJAMENTO DE PERFORMANCE">COORDENACAO PLANEJAMENTO DE PERFORMANCE</option>
+                <option value="COORDENACAO PROCESSOS LAMINACAO A QUENTE">COORDENACAO PROCESSOS LAMINACAO A QUENTE</option>
+                <option value="GERENCIA DE DESENVOLVIMENTO INDUSTRIAL">GERENCIA DE DESENVOLVIMENTO INDUSTRIAL</option>
+                <option value="GERENCIA GERAL DE METALURGIA DO ACO">GERENCIA GERAL DE METALURGIA DO ACO</option>
+                <option value="GERENCIA DE MANUTENCAO">GERENCIA DE MANUTENCAO</option>
+                <option value="GERENCIA QUALID PRODUTOS ASSIST TECNICA">GERENCIA QUALID PRODUTOS ASSIST TECNICA</option>
+                <option value="GERENCIA GERAL ENERGETICOS E UTILIDADES">GERENCIA GERAL ENERGETICOS E UTILIDADES</option>
+                <option value="COORDENACAO DE UTILIDADES">COORDENACAO DE UTILIDADES</option>
+                <option value="COORDENACAO DE LAMINACAO">COORDENACAO DE LAMINACAO</option>
+                <option value="COORDENACAO DE PCP FATURAMENTO E ACESSO">COORDENACAO DE PCP FATURAMENTO E ACESSO</option>
+                <option value="COORDENACAO DE PINTURA">COORDENACAO DE PINTURA</option>
+                <option value="DIRETORIA PROJETOS MANUTENCAO E SUPORTE">DIRETORIA PROJETOS MANUTENCAO E SUPORTE</option>
+                <option value="GERENCIA GERAL DE OPERACOES PARANA">GERENCIA GERAL DE OPERACOES PARANA</option>
+                <option value="GERENCIA GERAL DE OPERACOES PORTO REAL">GERENCIA GERAL DE OPERACOES PORTO REAL</option>
+                <option value="GERENCIA MANUT MAQ MOV E CORREIAS TRANSP">GERENCIA MANUT MAQ MOV E CORREIAS TRANSP</option>
+                <option value="GERENCIA DE COQUEIFICACAO">GERENCIA DE COQUEIFICACAO</option>
+                <option value="GERENCIA DE PCP EMBALAGEM E LOGISTICA">GERENCIA DE PCP EMBALAGEM E LOGISTICA</option>
+                <option value="GERENCIA DE MANUTENCAO">GERENCIA DE MANUTENCAO</option>
+                <option value="GERENCIA DE PROJETOS ALTO FORNO">GERENCIA DE PROJETOS ALTO FORNO</option>
+                <option value="GERENCIA CENTRO DE SERVICO E ZINCAGEM">GERENCIA CENTRO DE SERVICO E ZINCAGEM</option>
+                <option value="DIRETORIA DE PRODUTOS">DIRETORIA DE PRODUTOS</option>
+                <option value="GERENCIA DE MANUTENCAO MECANICA">GERENCIA DE MANUTENCAO MECANICA</option>
+                <option value="GERENCIA DE LINGOTAMENTO CONTINUO">GERENCIA DE LINGOTAMENTO CONTINUO</option>
+                <option value="GERENCIA GERAL DE TECNOLOGIA E PROJETOS">GERENCIA GERAL DE TECNOLOGIA E PROJETOS</option>
+                <option value="GERENCIA GERAL PRODUCAO ACOS LONGOS">GERENCIA GERAL PRODUCAO ACOS LONGOS</option>
+                <option value="GERENCIA GERAL SINTERIZACAO E ALTO FORNO">GERENCIA GERAL SINTERIZACAO E ALTO FORNO</option>
+                <option value="GERENCIA GERAL DE REDUTORES">GERENCIA GERAL DE REDUTORES</option>
+            </select>
+            <label for="csn">Não trabalho na CSN</label>
+            <input type="checkbox" id="csn" name="csn">
+            <button onclick="confirmar_3()">Continuar</button>
         </div>
     </div>
     <script src="../passagemDados.js"></script>
--- site2.0/passagemDados.js
+++ site2.0/passagemDados.js
@@ -4,6 +4,10 @@ const form_2 = document.getElementById("form_2_id");
 const form_3 = document.getElementById("form_3_id");
 function confirmar_1(){
     const resposta_form_1 = document.querySelector('input[name="resposta_form_1"]:checked');
+    if (resposta_form_1 === null){
+        console.log("---")
+        return;
+    }
     if (resposta_form_1.value === "n"){
         form.submit();
         return;
@@ -14,10 +18,27 @@ function confirmar_1(){
 
 function confirmar_2(){
     const resposta_form_2 = document.querySelector('input[name="resposta_form_2"]:checked');
+    if (resposta_form_2 === null){
+        console.log("---")
+        return;
+    }
     if (resposta_form_2.value === "n"){
         form.submit();
         return;
     }
     form_2.style.display = "none";
     form_3.style.display = "block";
+}
+
+function confirmar_3(){
+    const csn_checkbox = document.getElementById("csn");
+    if (csn_checkbox.checked){
+        form.unidade.value = "";
+    }else{
+        const departamento_selecionado = document.getElementById("departamentos");
+        form.unidade.value = departamento_selecionado.value;
+    }
+    const bairro_selecionado = document.getElementById("bairro_dropdown_id");
+    form.bairro.value = bairro_selecionado.value;
+    form.submit();
 }
